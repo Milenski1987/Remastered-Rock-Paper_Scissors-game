bb261d912071ea8a24ef77d99dee9dbc8ddd3f82
Milen Nikolov <milenski1987@icloud.com>
2025-01-31 23:52:37 +0200

    Update rock_paper_scissors_game.py

--- rock_paper_scissors_game.py
+++ rock_paper_scissors_game.py
@@ -1,14 +1,17 @@
-import random
+from random import randint
+from time import sleep
+
 
 games_count = 0
 computer_won_count = 0
 user_won_count = 0
 draws_count = 0
 stop_game = False
+print("Welcome to Rock, Paper, Scissors Game")
 
 while True:
     computer_choice = ""
-    computer_turn = random.randint(1, 3)
+    computer_turn = randint(1, 3)
     if computer_turn == 1:
         computer_choice = "rock"
     elif computer_turn == 2:
@@ -17,6 +20,7 @@ while True:
         computer_choice = "paper"
 
     user_choice = ""
+    sleep(1)
     user_turn = input("Please choose [r]ock, [s]cissors or [p]aper: ")
     if user_turn.lower() == "r":
         user_choice = "rock"
@@ -30,20 +34,32 @@ while True:
 
     if user_choice == computer_choice:
         print(f"User choice: {user_choice}")
+        sleep(1)
         print(f"Computer choice: {computer_choice}")
+        sleep(1)
         print(f"Result: Draw")
+        sleep(1)
+        input("Press enter to continue...")
         draws_count += 1
     elif (user_choice == "rock" and computer_choice == "scissors") \
             or (user_choice == "scissors" and computer_choice == "paper") \
             or (user_choice == "paper" and computer_choice == "rock"):
         print(f"User choice: {user_choice}")
+        sleep(1)
         print(f"Computer choice: {computer_choice}")
+        sleep(1)
         print(f"Result: User wins")
+        sleep(1)
+        input("Press enter to continue...")
         user_won_count += 1
     else:
         print(f"User choice: {user_choice}")
+        sleep(1)
         print(f"Computer choice: {computer_choice}")
+        sleep(1)
         print(f"Result: Computer wins")
+        sleep(1)
+        input("Press enter to continue...")
         computer_won_count += 1
 
     games_count += 1
